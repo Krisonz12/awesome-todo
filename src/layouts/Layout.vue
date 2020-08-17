412fa8ad622b83f4621a052e2aab69979fd4e429
Krisonz12 <onnezanya@gmail.com>
2020-08-17 14:10:51 +0100

    adding files

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title class="text-center">
+          Awesome Todo
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Navigation
+        </q-item-label>
+
+        <q-item
+          to="/"
+          exact
+          clickable>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Todo</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          to="/settings"
+          exact=
+          clickable>
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+    }
+  }
+}
+</script>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <p>Todo Page</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
